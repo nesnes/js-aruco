90577c67d7112e66fb357381b7aa18d1c6657047
Alexandre Brehmer <alexandre.brehmer@insa-rouen.fr>
2020-10-25 13:09:06 +0100

    WIP adding multi-marker size support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   CV: require('./src/cv'),
   AR: require('./src/aruco'),
+  AR4x4: require('./src/aruco4x4'),
   SVD: require('./src/svd'),
   POS1: require('./src/posit1'),
   POS2: require('./src/posit2')
